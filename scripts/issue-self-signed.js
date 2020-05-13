da6cb19029874513889e71013b167da054369b1f
stackshuffle <stackshuffle@gmail.com>
2020-05-13 13:15:06 -0600

    allow .env.example file

--- scripts/issue-self-signed.js
+++ scripts/issue-self-signed.js
@@ -8,10 +8,10 @@ shell.echo(`Removing old certs and root store from ${process.env.CAROOT}/ direct
 shell.exec(`mkcert -uninstall`);
 shell.exec(`npx rimraf $(pwd)/ssl/certs/*`);
 
-shell.echo(`Updating root CA store location...`)
+shell.echo(`Updating root CA store location...`);
 shell.exec(`mkcert -install`);
 
 shell.echo(`Creating new certificates for "localhost"...`);
-shell.exec(`mkcert -client -key-file ${process.env.CAROOT}/localhost.key -cert-file ${process.env.CAROOT}/localhost.crt localhost "*.localhost.dev" 127.0.0.1 ::1`);
+shell.exec(`mkcert -key-file ${process.env.CAROOT}/localhost.key -cert-file ${process.env.CAROOT}/localhost.crt localhost "*.localhost" 127.0.0.1 ::1`);
 
 shell.echo(`Done!`);
